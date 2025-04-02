a688a815023569bb71230d1e5b313463c9b072f2
Cole Sullivan <47622215+cole-sullivan@users.noreply.github.com>
2025-04-02 21:21:47 +0000

    first commit

---
+++ root.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Set root password (user will have to enter this)
+passwd
+
+# Install additional packages, the kernel, and firmware
+pacman -S base-devel dosfstools grub efibootmgr lvm2 mtools neovim networkmanager os-prober sudo
+pacman -S linux linux-headers linux-firmware
+pacman -S mesa intel-media-driver
+
+# Generate RAM disks
+rm -f /etc/mkinitcpio.conf
+curl -LO https://raw.githubusercontent.com/cole-sullivan/live-arch-helper/main/mkinitcpio.conf
+chmod 644 mkinitcpio.conf
+mv mkinitcpio.conf /etc/mkinitcpio.conf
+mkinitcpio -p linux
+
+# Set locale
+rm -f /etc/locale.gen
+curl -LO https://raw.githubusercontent.com/cole-sullivan/live-arch-helper/main/locale.gen
+chmod 644 locale.gen
+mv locale.gen /etc/locale.gen
+locale-gen
+
+# Configure grub
+rm -f /etc/default/grub
+curl -LO https://raw.githubusercontent.com/cole-sullivan/live-arch-helper/main/grub
+chmod 644 grub
+mv grub /etc/default/grub
+mkdir /boot/EFI
+mount /dev/nvme0n1p1 /boot/EFI
+grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Enable services
+systemctl enable NetworkManager
+
+exit
---
+++ setup.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# Partition disk
+(echo g; echo w) | fdisk /dev/nvme0n1
+(echo n; echo 1; echo ""; echo +1G; echo p; echo n; echo 2; echo ""; echo +1G; echo p; echo n; echo 3; echo ""; echo ""; echo t; echo 3; echo 44; echo p; echo w) | fdisk /dev/nvme0n1
+
+# Format partitions
+mkfs.fat -F32 /dev/nvme0n1p1
+mkfs.ext4 /dev/nvme0n1p2
+cryptsetup luksFormat /dev/nvme0n1p3 # User needs to enter password 2x
+cryptsetup open --type luks /dev/nvme0n1p3 lvm # User needs to enter password again
+pvcreate /dev/mapper/lvm
+vgcreate volgroup0 /dev/mapper/lvm
+lvcreate -L 30GB volgroup0 -n lv_root
+lvcreate -l 100%FREE volgroup0 -n lv_home
+modprobe dm_mod
+vgscan
+vgchange -ay
+mkfs.ext4 /dev/volgroup0/lv_root
+mkfs.ext4 /dev/volgroup0/lv_home
+
+# Mount partitions
+mount /dev/volgroup0/lv_root /mnt
+mkdir /mnt/boot
+mount /dev/nvme0n1p2 /mnt/boot
+mkdir /mnt/home
+mount /dev/volgroup0/lv_home /mnt/home
+
+# Install base packages
+pacstrap -i /mnt base
+
+# Generate fstab
+genfstab -U -p /mnt >> /mnt/etc/fstab
+cat /mnt/etc/fstab
+
+# Enter system and run rest of script
+curl -LO https://raw.githubusercontent.com/cole-sullivan/live-arch-helper/main/root.sh
+sh root.sh | arch-chroot /mnt
+
+# Unmount all partitions and exit live USB
+umount -a
+reboot
